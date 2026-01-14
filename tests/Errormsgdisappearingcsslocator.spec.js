a607e044939645212da09ead0e7c52c033107fb3
viowb <violet.windebank@gmail.com>
2026-01-14 11:35:46 +0000

    commit 2

--- tests/Errormsgdisappearingcsslocator.spec.js
+++ tests/Errormsgdisappearingcsslocator.spec.js
@@ -3,14 +3,27 @@ const { error } = require('node:console');
 
 
 test.only('Login Page error msg Playwright Test', async ({ page }) => {
+  
+
   await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
   await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
-  await page.locator('#username').fill('rahulshettyacademy');
-  await page.locator('#password').fill('learnings');
-  await page.locator('#signInBtn').click(); 
+  
+  const userName = page.locator('#username');
+  const password = page.locator('#password');
+  const signInBtn = page.locator('#signInBtn');
+  await userName.fill('rahulshettyacademy');
+  await password.fill('learnings');
+  await signInBtn.click(); 
 
   console.log(await page.locator('[style*="block"]').textContent());
   await expect(page.locator('[style*="block"]')).toHaveText('Incorrect username/password.');
 
+  await userName.fill("");
+  await userName.fill("viowbank@gmail.com");
+  await password.fill("");
+
+  await password.fill("T5yhhgdjsdkls;");
+  await signInBtn.click();
+ 
 
 });
